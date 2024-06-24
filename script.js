812c995aee5da2fd5443fb80210c21cf8098881a
Carlo Eduardo <carloedu010@gmail.com>
2024-06-24 18:42:00 -0300

    carrousel

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ function menu(){
     const menu = document.querySelector("nav .hamburguer")
     menu.style.maxHeight = menu.style.maxHeight == "0px"? "577px": "0"
 }
+window.addEventListener("resize", () => {
+    const menu = document.querySelector("nav .hamburguer")
+    if (innerWidth > 600){
+        menu.style.maxHeight = 0
+    }
+})
+
+const carrousel = document.querySelector(".carrousel .images")
+const carrousel_imgs = document.querySelectorAll(".carrousel .images img")
+
+let current = 1
+
+function CarrouselNext(){
+        current++
+        CarrouselMove()
+}
+function CarrouselMove(animation=true){
+    if (animation) carrousel.style.transition = "all .5s ease-in-out"
+    else carrousel.style.transition = "none"
+    carrousel.style.transform = `translateX(-${current * 350}px)`
+    setTimeout(() =>{
+    if (current == carrousel_imgs.length - 1){
+        current = 1
+        CarrouselMove(false)
+    }
+    }, 500)
+}
+let interval = setInterval(CarrouselNext, 5000)
